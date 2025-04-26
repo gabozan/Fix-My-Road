f729c727e49e58d4872107733f35f9ba9a708b77
Gabriel Rios <1671177@uab.cat>
2025-04-26 18:50:46 +0200

    primer commit deep learning

---
+++ deep_learning/config.py
@@ -0,0 +1,15 @@
+# Ruta al modelo preentrenado
+MODEL_PATH = "../models/yolov8n.pt"
+
+# Rutas al dataset
+DATA_YAML = "../data/processed_dl/data.yaml"
+
+# Tamaño de imagen
+IMG_SIZE = 512
+
+# Número de clases
+NUM_CLASSES = 1  # por ejemplo, imperfecciones
+
+# Hiperparámetros de entrenamiento
+EPOCHS = 50
+BATCH_SIZE = 16
