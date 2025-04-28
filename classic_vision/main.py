4dbf46d1c9c1d450b6121fa22e180694f7fba391
NIU1672001 <NIU1672001@uab.cat>
2025-04-28 02:29:07 +0200

    Detecció de carretera fent ús de tècniques clàsiques

--- classic_vision/main.py
+++ classic_vision/main.py
@@ -1,7 +1,13 @@
-import functions
+from functions import *
 
-if __name__ == '__main__':
-    carpeta_imagenes = '../data/train/img'
-    imagenes_procesadas = load_and_preprocess_images(carpeta_imagenes)
+if __name__ == "__main__":
+    input_folder = "../data/processed_cv/train"
+    output_folder_cv = "./results_cv"
+    n_samples = 10
 
-    pass
+    hsv_min = np.array([0, 0, 30], dtype=np.uint8)
+    hsv_max = np.array([180, 70, 220], dtype=np.uint8)
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
+    green = (0, 255, 0)
+
+    process_images(input_folder, output_folder_cv, n_samples, hsv_min, hsv_max, kernel, green)
