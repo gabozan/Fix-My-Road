a8782f13399376d7c2cb88396dde0a63bbc1c9bb
Gabriel Rios <GabrielAbsalon.Rios@autonoma.cat>
2025-05-05 09:14:29 +0200

    deep learning final

--- deep_learning/pipeline.py
+++ deep_learning/pipeline.py
@@ -1,29 +1,46 @@
 from pathlib import Path
 import csv
+from utils import load_image, save_image
+from preprocessing import letterbox
 from model import load_model
 from predict import predict_image
+
 
 ########################################################################################################################
 
 MODEL_PATH = "theRoadFixer.pt"
-IMAGES_DIR = Path("../data/processed_dl/test/")
+INPUT_DIR = Path("../data/raw_reduced/test/")
+PROCESSED_DIR = Path("../data/processed_dl/test/")
 OUTPUT_DIR = Path("../predictions/")
 CSV_PATH = OUTPUT_DIR / "predictions_dl.csv"
 
 ########################################################################################################################
 
+PROCESSED_DIR.mkdir(parents=True, exist_ok=True)
+OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
+
 model = load_model(MODEL_PATH)
 csv_rows = []
 
-for image_file in IMAGES_DIR.glob("*.jpg"):
+for image_file in INPUT_DIR.glob("*.jpg"):
     file_name = image_file.stem
-    results = predict_image(model, image_file)
+    img = load_image(image_file)
+    original_size = (img.shape[1], img.shape[0])
+    img_resized, scale, (left, top) = letterbox(img, original_size)
+    save_image((PROCESSED_DIR / f"{file_name}.jpg"), img_resized)
+    results = predict_image(model, (PROCESSED_DIR / f"{file_name}.jpg"))
     boxes = results[0].boxes
 
     predictions = []
     for i in range(len(boxes)):
         class_id = boxes.cls[i]
-        x1, y1, x2, y2 = boxes.xyxy[i].tolist()
+        x1, y1, x2, y2 = map(float, boxes.xyxy[i].tolist())
+
+        x1 = (x1 - left) / scale
+        y1 = (y1 - top) / scale
+        x2 = (x2 - left) / scale
+        y2 = (y2 - top) / scale
+
         predictions.extend([int(class_id), int(x1), int(y1), int(x2), int(y2)])
 
     prediction_str = " ".join(map(str, predictions))
@@ -32,7 +49,6 @@ for image_file in IMAGES_DIR.glob("*.jpg"):
         "PredictionString": prediction_str
     })
 
-OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
 with CSV_PATH.open("w", newline="") as f:
     writer = csv.DictWriter(f, fieldnames=["ImageId", "PredictionString"])
     writer.writeheader()
